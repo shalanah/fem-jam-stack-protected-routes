3c71ad761be45f7099625b2acf16e61e54cf8b2c
shalanah <shalanahfaith@gmail.com>
2020-06-19 21:19:33 -0500

    Add identity context provider

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "gatsby";
+import { IdentityContextProvider } from "react-netlify-identity-widget";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <IdentityContextProvider
+      url={"https://jamstack-intro-auth-shalanah.netlify.com"}
+    >
       <header>
         <Link to="/">Jamstack App</Link>
         <main>{children}</main>
       </header>
-    </>
+    </IdentityContextProvider>
   );
 };
 
