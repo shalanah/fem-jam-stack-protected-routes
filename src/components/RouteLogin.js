5b00b247830099a8b387d23fb7b2df3f15555421
shalanah <shalanahfaith@gmail.com>
2021-05-06 11:25:24 -0500

    In-progress

--- src/components/RouteLogin.js
+++ src/components/RouteLogin.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useIdentityContext } from "react-netlify-identity";
+import { navigate } from "gatsby";
 
 const RouteLogin = () => {
+  const identity = useIdentityContext();
+  console.log(identity);
+  if (identity && identity.isLoggedIn) {
+    navigate("/dashboard/secret", { replace: true });
+  }
   return (
     <>
       <h1>Login or Sign up</h1>
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import { navigate } from "gatsby";
+import IdentityModal from "react-netlify-identity-widget";
 import { Router } from "@reach/router";
 import Layout from "../components/Layout";
 import Profile from "./profile";
 import RouteBase from "../components/RouteBase";
 import RouteSecret from "../components/RouteSecret";
 import RouteLogin from "../components/RouteLogin";
+
+import "react-netlify-identity-widget/styles.css";
+import "@reach/tabs/styles.css";
 
 const Dashboard = ({
   location, // comes with Gatsby, lucky us!
@@ -23,6 +27,7 @@ const Dashboard = ({
         <RouteSecret path="/dashboard/secret" />
         <RouteLogin path="/dashboard/login" />
       </Router>
+      <IdentityModal showDialog={true} ariaLabel={"Sign-in and up form"} />
     </Layout>
   );
 };
